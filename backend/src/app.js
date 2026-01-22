4d748be9a6fac5e72a52384aa387e5efb5eb3000
pranavpathak08 <ppathak2002@gmail.com>
2026-01-22 13:43:56 +0530

    adding routes/auth.routes.js and mounting in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import authRoutes from './routes/auth.routes.js'
 
 const app = express();
 
@@ -9,6 +10,9 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send("Welcome to Backend");
 })
+
+//Mounting Routes
+app.use('/api/auth', authRoutes);
 
 export default app;
 
