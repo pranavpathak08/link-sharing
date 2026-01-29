4ce8d1a0970e460ba44e8652ddf0438f1f764fd2
pranavpathak08 <ppathak2002@gmail.com>
2026-01-29 11:26:45 +0530

    small fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -32,7 +32,7 @@ app.use((err, req, res, next) => {
     console.error("Global error: ", err);
     res.status(err.status || 500).json({
         message: err.message || "Internal Server Error",
-        ...(process.env.NODE_ENV === 'development' && { stack: err.stack });
+        ...(process.env.NODE_ENV === 'development' && { stack: err.stack })
     })
 })
 
